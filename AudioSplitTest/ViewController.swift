0c26c6d6761902dacd59e8bb12119230df06f2f5
Akira Matsuda <akira.matsuda@me.com>
2017-12-27 13:22:54 +0900

    initial commit

--- AudioSplitTest/ViewController.swift
+++ AudioSplitTest/ViewController.swift
@@ -7,12 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+//		AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:openPanel.URL options:nil];
+//		NSLog(@"Tracks: %@", asset.tracks);
+		var urlString = Bundle.main.path(forResource: "IMG_1785", ofType: "MOV")
+		var url = URL(fileURLWithPath: urlString!)
+		let asset1 = AVURLAsset.init(url: url)
+		print(asset1.tracks)
+		
+		urlString = Bundle.main.path(forResource: "IMG_1786", ofType: "MOV")
+		url = URL(fileURLWithPath: urlString!)
+		let asset2 = AVURLAsset.init(url: url)
+		
+		let composition = AVMutableComposition()
+		let audioTrack = asset1.tracks(withMediaType: .audio)[0]
+		let videoTrack = asset2.tracks(withMediaType: .video)[0]
+		
+		do {
+			let range = CMTimeRangeMake(kCMTimeZero, asset1.duration)
+			let videoTrackComposition = composition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+			try videoTrackComposition?.insertTimeRange(CMTimeRangeMake(kCMTimeZero, videoTrack.timeRange.duration), of: videoTrack, at: kCMTimeZero)
+			
+			let instruction = AVMutableVideoCompositionInstruction()
+			instruction.timeRange = range
+			let layerInstruction = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrackComposition!)
+			
+			let audioTrackComposition = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+			try audioTrackComposition?.insertTimeRange(range, of: audioTrack, at: kCMTimeZero)
+			
+			instruction.layerInstructions = [layerInstruction]
+			let videoComposition = AVMutableVideoComposition()
+			videoComposition.instructions = [instruction]
+			videoComposition.renderSize = videoTrack.naturalSize
+			videoComposition.frameDuration = CMTimeMake(1, 30)
+			
+			let outputPath = NSHomeDirectory() + "/Documents" + "/out.mp4"
+			let session = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetHighestQuality)
+			session?.outputURL = URL(fileURLWithPath: outputPath)
+			session?.outputFileType = AVFileType.m4v
+			session?.videoComposition = videoComposition
+			session?.exportAsynchronously(completionHandler: {
+				print("done")
+				print(outputPath)
+			})
+		}
+		catch {
+			print("exception")
+		}
+
+		
 	}
 
 	override func didReceiveMemoryWarning() {
